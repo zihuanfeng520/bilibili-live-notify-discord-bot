039133fcc52dfa768044bde2839702ee2d843cbe
王英瑋 <zihuanfeng520@gmail.com>
2024-07-15 18:43:53 +0800

    Update test.js

--- test.js
+++ test.js
@@ -49,10 +49,10 @@ async function checkLiveStatuses() {
         currentStatuses[roomId] = status; // 更新當前狀態
 
         if (status === ROOM_STATUS.ONLINE) {
-          const message = `直播已開始！房間標題：${room_info.title}\n\n房間鏈接：https://live.bilibili.com/${room_info.room_id}\n\n[封面連結](${room_info.cover})`;
+          const message = `直播已開始！房間標題：${room_info.title}\n\n房間鏈接：https://live.bilibili.com/${room_info.room_id}\n\n[封面連結](${room_info.cover})\n`;
           await sendToAllChannels(message);
         } else {
-          const message = `直播已結束！房間標題：${room_info.title}\n\n房間鏈接：https://live.bilibili.com/${room_info.room_id}\n\n[封面連結](${room_info.cover})`;
+          const message = `直播已結束！房間標題：${room_info.title}\n\n房間鏈接：https://live.bilibili.com/${room_info.room_id}\n\n[封面連結](${room_info.cover})\n`;
           await sendToAllChannels(message);
         }
       }
